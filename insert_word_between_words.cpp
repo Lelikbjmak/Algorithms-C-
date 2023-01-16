13296a87a7db812426d2cc11b955a6435fd4f556
Denis Solovey <109223818+Lelikbjmak@users.noreply.github.com>
2023-01-16 17:20:00 +0300

    Inser 'Visual' between 2nd and 3rd word

---
+++ insert_word_between_words.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<string>
+#include<string>
+
+using namespace std;
+
+// Между 2ым и 3им словом вставить Visual
+
+int main()
+{
+	char a[255];
+	char vs[255];
+	int k = 0;
+
+	puts("Input the string");
+
+	gets_s(a);
+
+	int count = 0;
+	for (int i = 1; a[i] != '\0'; i++)
+	{
+		if (a[i] == ' ' )
+			count++;
+
+		if (count == 2)
+		{
+			k = i + 1;
+			break;
+		}
+	}
+
+
+
+	cout << "Index is " << k;
+
+	
+
+
+	
+
+	return 0;
+}
+
