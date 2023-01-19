d725dc762a56913c13d6fde3b69119049f9903f1
Denis Solovey <109223818+Lelikbjmak@users.noreply.github.com>
2023-01-19 11:10:39 +0300

    Max in a raw, min in column elems

---
+++ elems_max_in_a_raw_and_min_in_column.cpp
@@ -0,0 +1,85 @@
+﻿#include<iostream>
+
+using namespace std;
+
+// найти элементы которые максимальные в строке и минимальные в столбце 
+
+
+int main()
+{
+
+	int m, n;
+	cin >> m >> n;
+
+
+	int** a;
+	a = new int* [m];
+	for (int i = 0; i < m; i++)
+	{
+		a[i] = new int[n];
+		for (int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]= ";
+			cin >> a[i][j];
+		}
+	}
+
+
+	cout << "\n";
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << "  ";
+
+		}
+		cout << endl;
+
+	}
+
+	cout << "\n";
+
+
+	int* b = new int[m + n];
+
+	int max;
+
+	int count = 0;
+	int c = 0;
+	for (int j = 0; j < n; j++)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			max = a[i][j];
+
+			for (int p = 0; p < m; p++)
+			{
+				if (a[p][j] > max)
+				{
+					max = a[p][j];
+				}
+
+			} 
+			while (p!=i)
+
+			
+				
+
+				
+				
+					
+				
+
+		}
+	}
+
+	for (int i = 0; i < count; i++)
+		cout << b[i] << "   ";
+
+	return 0;
+
+}
+
+
+
