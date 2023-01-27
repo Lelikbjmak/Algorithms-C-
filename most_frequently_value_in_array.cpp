638eaf54008bdbb70f1b71630ff0c7b531c0fc07
Denis Solovey <109223818+Lelikbjmak@users.noreply.github.com>
2023-01-28 00:10:06 +0300

    most frequently value in array

---
+++ most_frequently_value_in_array.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+                       // В массиве найти число, которое встречается чаще всего 
+int main()
+
+{
+
+	int m;
+	cout << "Enter the amount of elements ";
+	cin >> m;
+
+	int* a = new int[m];
+
+	for (int i = 0; i < m; i++)
+	{
+		cout << "a[" << i << "]= ";
+		cin >> a[i];
+
+	}
+
+	cout << "\n\n";
+
+	for (int i = 0; i < m; i++)
+		cout << a[i] << "  ";
+
+	int count=0; int b = 0;
+	for (int i = 0; i < m-1; i++)
+		for (int j = i + 1; j < m; j++)
+		{
+			if (int p=(a[i] == a[j]))
+			{
+				cout <<"p=" << p << endl;
+				b = p;
+			}
+	    }
+
+	for (int i = 0; i < m; i++)
+	{
+		if (a[i] == b)
+			count++;
+	}
+
+	cout << "\n\n";
+
+	cout << "Most common element is " << b << endl;
+	cout << "The amount of this element is " << count << endl;
+
+	return 0;
+}
